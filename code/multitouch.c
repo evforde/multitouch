1a879edfc4a0a2e99d2a1e2b7cf35da2d8305aec
Elliott Forde <eforde@mit.edu>
2018-11-28 00:59:21 -0500

    Slightly more working

--- code/multitouch.c
+++ code/multitouch.c
@@ -8,22 +8,15 @@
 
 #define TIMEOUT_TIME 5000
 
-static volatile unsigned char* const send_ports[12] = {&PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTB, &PORTB, &PORTB, &PORTC, &PORTC};
-static volatile unsigned char* const send_ddrs[12] = {&DDRD, &DDRD, &DDRD, &DDRD, &DDRD, &DDRD, &DDRD, &DDRB, &DDRB, &DDRB, &DDRC, &DDRC};
-static unsigned const char const send_pins[12] = {PD0, PD2, PD3, PD4, PD5, PD6, PD7, PB0, PB1, PB2, PC0, PC1};
-
 // not including ADC6 and ADC7 because they cannot be manipulated in software
-static unsigned const char const receive_pins[4] = {PC2, PC3, PC4, PC5};
-// specify which pin to use as the ADC; ADLAR set for all
-// TODO: mess around with AREF settings
-static unsigned const char const receive_muxes[6] = {0b01100110, 0b01100111, 0b01100010, 0b01100011, 0b01100100, 0b01100101};
+static unsigned char receive_pins[4] = {PC2, PC3, PC4, PC5};
 
 
-void scan_pad(uint8_t send_index, uint8_t receive_index) {
-    volatile unsigned char* sendport = send_ports[send_index];
-    volatile unsigned char* sendddr = send_ddrs[send_index];
-    unsigned char sendpin = send_pins[send_index];
-    unsigned char muxval = receive_muxes[receive_index];
+void scan_pad(
+        volatile unsigned char* sendport,
+        volatile unsigned char* sendddr,
+        unsigned char sendpin,
+        unsigned char muxval) {  
     // Wait for the receive pin to settle
     ADMUX = muxval;
     
@@ -60,6 +53,7 @@ void scan_pad(uint8_t send_index, uint8_t receive_index) {
     for (i = 0; i < 4; i++) {
         set(DDRC, receive_pins[i]);
     }
+    _delay_ms(1);
 }
 
 int main(void)
@@ -74,23 +68,96 @@ int main(void)
     // Initialize various special features
     serial_init();
     
+    static volatile unsigned char* send_ports[12] = {&PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTD, &PORTB, &PORTB, &PORTB, &PORTC, &PORTC};
+    static volatile unsigned char* send_ddrs[12] = {&DDRD, &DDRD, &DDRD, &DDRD, &DDRD, &DDRD, &DDRD, &DDRB, &DDRB, &DDRB, &DDRC, &DDRC};
+    static unsigned char send_pins[12] = {PD0, PD2, PD3, PD4, PD5, PD6, PD7, PB0, PB1, PB2, PC0, PC1};
+    
+    // specify which pin to use as the ADC
+    // ADLAR set for all
+    // TODO: mess around with AREF settings
+    static unsigned char receive_muxes[6] = {0b01100110, 0b01100111, 0b01100010, 0b01100011, 0b01100100, 0b01100101};
+    
+    // for some reason, all outputs only show up when shooting out of PA3
     while(1) {
         put_char(&serial_port, serial_pin_out, 'h');
         put_char(&serial_port, serial_pin_out, 'e');
         put_char(&serial_port, serial_pin_out, 'l');
         put_char(&serial_port, serial_pin_out, 'l');
         put_char(&serial_port, serial_pin_out, 'o');
-        uint8_t receive_index, send_index;
-        for (receive_index = 0; receive_index < 6; receive_index++) {
-            for (send_index = 0; send_index < 1; send_index++) {
-                put_char(&serial_port, serial_pin_out, receive_index);
-                put_char(&serial_port, serial_pin_out, receive_muxes[0]);
-                put_char(&serial_port, serial_pin_out, receive_muxes[(int)receive_index]);
-                uint8_t is_match = receive_index == 0;
-                put_char(&serial_port, serial_pin_out, is_match);
-                scan_pad(send_index, receive_index);
-            }
-        }
+        // #TODO: try iterating through these in a different order
+        // #TODO: try putting a diode between the resistor and touchpad
+        scan_pad(send_ports[0], send_ddrs[0], send_pins[0], receive_muxes[0]);
+        scan_pad(send_ports[0], send_ddrs[0], send_pins[0], receive_muxes[1]);
+        scan_pad(send_ports[0], send_ddrs[0], send_pins[0], receive_muxes[2]);
+        scan_pad(send_ports[0], send_ddrs[0], send_pins[0], receive_muxes[3]);
+        scan_pad(send_ports[0], send_ddrs[0], send_pins[0], receive_muxes[4]);
+        scan_pad(send_ports[0], send_ddrs[0], send_pins[0], receive_muxes[5]);
+        scan_pad(send_ports[1], send_ddrs[1], send_pins[1], receive_muxes[0]);
+        scan_pad(send_ports[1], send_ddrs[1], send_pins[1], receive_muxes[1]);
+        scan_pad(send_ports[1], send_ddrs[1], send_pins[1], receive_muxes[2]);
+        scan_pad(send_ports[1], send_ddrs[1], send_pins[1], receive_muxes[3]);
+        scan_pad(send_ports[1], send_ddrs[1], send_pins[1], receive_muxes[4]);
+        scan_pad(send_ports[1], send_ddrs[1], send_pins[1], receive_muxes[5]);
+        scan_pad(send_ports[2], send_ddrs[2], send_pins[2], receive_muxes[0]);
+        scan_pad(send_ports[2], send_ddrs[2], send_pins[2], receive_muxes[1]);
+        scan_pad(send_ports[2], send_ddrs[2], send_pins[2], receive_muxes[2]);
+        scan_pad(send_ports[2], send_ddrs[2], send_pins[2], receive_muxes[3]);
+        scan_pad(send_ports[2], send_ddrs[2], send_pins[2], receive_muxes[4]);
+        scan_pad(send_ports[2], send_ddrs[2], send_pins[2], receive_muxes[5]);
+        scan_pad(send_ports[3], send_ddrs[3], send_pins[3], receive_muxes[0]);
+        scan_pad(send_ports[3], send_ddrs[3], send_pins[3], receive_muxes[1]);
+        scan_pad(send_ports[3], send_ddrs[3], send_pins[3], receive_muxes[2]);
+        scan_pad(send_ports[3], send_ddrs[3], send_pins[3], receive_muxes[3]);
+        scan_pad(send_ports[3], send_ddrs[3], send_pins[3], receive_muxes[4]);
+        scan_pad(send_ports[3], send_ddrs[3], send_pins[3], receive_muxes[5]);
+        scan_pad(send_ports[4], send_ddrs[4], send_pins[4], receive_muxes[0]);
+        scan_pad(send_ports[4], send_ddrs[4], send_pins[4], receive_muxes[1]);
+        scan_pad(send_ports[4], send_ddrs[4], send_pins[4], receive_muxes[2]);
+        scan_pad(send_ports[4], send_ddrs[4], send_pins[4], receive_muxes[3]);
+        scan_pad(send_ports[4], send_ddrs[4], send_pins[4], receive_muxes[4]);
+        scan_pad(send_ports[4], send_ddrs[4], send_pins[4], receive_muxes[5]);
+        scan_pad(send_ports[5], send_ddrs[5], send_pins[5], receive_muxes[0]);
+        scan_pad(send_ports[5], send_ddrs[5], send_pins[5], receive_muxes[1]);
+        scan_pad(send_ports[5], send_ddrs[5], send_pins[5], receive_muxes[2]);
+        scan_pad(send_ports[5], send_ddrs[5], send_pins[5], receive_muxes[3]);
+        scan_pad(send_ports[5], send_ddrs[5], send_pins[5], receive_muxes[4]);
+        scan_pad(send_ports[5], send_ddrs[5], send_pins[5], receive_muxes[5]);
+        scan_pad(send_ports[6], send_ddrs[6], send_pins[6], receive_muxes[0]);
+        scan_pad(send_ports[6], send_ddrs[6], send_pins[6], receive_muxes[1]);
+        scan_pad(send_ports[6], send_ddrs[6], send_pins[6], receive_muxes[2]);
+        scan_pad(send_ports[6], send_ddrs[6], send_pins[6], receive_muxes[3]);
+        scan_pad(send_ports[6], send_ddrs[6], send_pins[6], receive_muxes[4]);
+        scan_pad(send_ports[6], send_ddrs[6], send_pins[6], receive_muxes[5]);
+        scan_pad(send_ports[7], send_ddrs[7], send_pins[7], receive_muxes[0]);
+        scan_pad(send_ports[7], send_ddrs[7], send_pins[7], receive_muxes[1]);
+        scan_pad(send_ports[7], send_ddrs[7], send_pins[7], receive_muxes[2]);
+        scan_pad(send_ports[7], send_ddrs[7], send_pins[7], receive_muxes[3]);
+        scan_pad(send_ports[7], send_ddrs[7], send_pins[7], receive_muxes[4]);
+        scan_pad(send_ports[7], send_ddrs[7], send_pins[7], receive_muxes[5]);
+        scan_pad(send_ports[8], send_ddrs[8], send_pins[8], receive_muxes[0]);
+        scan_pad(send_ports[8], send_ddrs[8], send_pins[8], receive_muxes[1]);
+        scan_pad(send_ports[8], send_ddrs[8], send_pins[8], receive_muxes[2]);
+        scan_pad(send_ports[8], send_ddrs[8], send_pins[8], receive_muxes[3]);
+        scan_pad(send_ports[8], send_ddrs[8], send_pins[8], receive_muxes[4]);
+        scan_pad(send_ports[8], send_ddrs[8], send_pins[8], receive_muxes[5]);
+        scan_pad(send_ports[9], send_ddrs[9], send_pins[9], receive_muxes[0]);
+        scan_pad(send_ports[9], send_ddrs[9], send_pins[9], receive_muxes[1]);
+        scan_pad(send_ports[9], send_ddrs[9], send_pins[9], receive_muxes[2]);
+        scan_pad(send_ports[9], send_ddrs[9], send_pins[9], receive_muxes[3]);
+        scan_pad(send_ports[9], send_ddrs[9], send_pins[9], receive_muxes[4]);
+        scan_pad(send_ports[9], send_ddrs[9], send_pins[9], receive_muxes[5]);
+        scan_pad(send_ports[10], send_ddrs[10], send_pins[10], receive_muxes[0]);
+        scan_pad(send_ports[10], send_ddrs[10], send_pins[10], receive_muxes[1]);
+        scan_pad(send_ports[10], send_ddrs[10], send_pins[10], receive_muxes[2]);
+        scan_pad(send_ports[10], send_ddrs[10], send_pins[10], receive_muxes[3]);
+        scan_pad(send_ports[10], send_ddrs[10], send_pins[10], receive_muxes[4]);
+        scan_pad(send_ports[10], send_ddrs[10], send_pins[10], receive_muxes[5]);
+        scan_pad(send_ports[11], send_ddrs[11], send_pins[11], receive_muxes[0]);
+        scan_pad(send_ports[11], send_ddrs[11], send_pins[11], receive_muxes[1]);
+        scan_pad(send_ports[11], send_ddrs[11], send_pins[11], receive_muxes[2]);
+        scan_pad(send_ports[11], send_ddrs[11], send_pins[11], receive_muxes[3]);
+        scan_pad(send_ports[11], send_ddrs[11], send_pins[11], receive_muxes[4]);
+        scan_pad(send_ports[11], send_ddrs[11], send_pins[11], receive_muxes[5]);
         // _delay_ms(12.1);
         // _delay_ms(3.76);
     }
--- site/index.js
+++ site/index.js
@@ -1,9 +1,8 @@
 var ws = new WebSocket("ws://localhost:8765");
 
-var DEFAULT_VALUES = [
-  38, 38, 42, 39, 38, 40, 41, 39, 43, 39, 39, 38, 43, 43, 47, 43, 43, 44]
+var DEFAULT_VALUES = [105, 116, 106, 105, 105, 105, 105, 115, 105, 105, 104, 104, 109, 121, 110, 110, 109, 109, 107, 117, 107, 108, 106, 106, 106, 117, 107, 107, 106, 106, 108, 116, 107, 107, 106, 105, 106, 113, 103, 103, 103, 103, 106, 116, 105, 105, 105, 105, 104, 115, 104, 104, 104, 104, 103, 114, 104, 103, 103, 102, 103, 115, 103, 104, 102, 103, 101, 112, 102, 102, 101, 101];
 
-var MAX_DIFFERENCE = 8;
+var MAX_DIFFERENCE = 20;
 var MIN_OPACITY = 0.2;
 
 ws.onopen = function () {
@@ -20,7 +19,13 @@ ws.onmessage = function (e) {
     let row = Math.trunc(i / 6);
     let column = Math.trunc(i % 6);
     let difference = DEFAULT_VALUES[i] - readings[i];
-    let opacity = Math.max(difference / MAX_DIFFERENCE, 0) + MIN_OPACITY;
+    /*if (difference > 3)
+      difference = 8;
+    else
+      difference = 0;*/
+    difference = Math.min(difference, MAX_DIFFERENCE);
+    difference = difference * difference;
+    let opacity = Math.max(difference / (MAX_DIFFERENCE * MAX_DIFFERENCE), 0) + MIN_OPACITY;
     opacity = Math.min(opacity, 1);
     let selector = ".row:nth(" + row + ") .cell:nth(" + column +")";
     $(selector).css("opacity", opacity);
