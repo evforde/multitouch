06ae4530191dcb3f38825474caede054a2aeae64
Elliott Forde <eforde@mit.edu>
2018-12-17 20:27:57 -0500

    Update server

--- server.py
+++ server.py
@@ -38,14 +38,13 @@ def send(message):
     message = f"{message};"
     SOCKET.send(message.encode("utf-8"))
 
-def play_chord(*notes):
-    send(" ".join(notes))
+def play_chord(*fret_numbers):
+    send(" ".join(fret_numbers))
 
 def reverse(string):
     return "".join(reversed(string))
 
 buf = []
-base_notes = [67, 60, 64, 69]
 while True:
     wait = ser.inWaiting()
     if (wait != 0):
@@ -59,9 +58,8 @@ while True:
             hello = buf[1:]
             readings = buf[1:]
             binary = ["1" + reverse(format(ord(c), '#008b')[2:]) for c in readings]
+            fret_numbers = [str(c.rindex("1")) for c in binary]
             print(binary)
-            fret_numbers = [c.rindex("1") for c in binary]
-            notes = [str(fret + base) for fret, base in zip(fret_numbers, base_notes)]
-            print(notes)
+            print(fret_numbers)
+            play_chord(*fret_numbers)
             buf = []
-            play_chord(*notes)
